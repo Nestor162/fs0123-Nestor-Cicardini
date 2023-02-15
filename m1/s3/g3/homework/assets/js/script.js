f1d41fbd2bcd4014773ac82532a3628108f85524
Nestor162 <nestorcicardini@gmail.com>
2023-02-15 15:16:26 +0100

    Todo list primi passi

---
+++ m1/s3/g3/homework/assets/js/script.js
@@ -0,0 +1,15 @@
+let button = document.getElementById("add");
+let container = document.getElementById("task_container");
+
+button.addEventListener("click", getTask);
+
+function getTask() {
+    let task = document.getElementById("input").value;
+    addTask(task);
+}
+
+function addTask(currentTask) {
+    let li = document.createElement("li");
+    li.innerText = currentTask;
+    container.append(li);
+}
