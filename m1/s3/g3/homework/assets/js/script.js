9de92e4d735b6167b6c19924f16c82b362210ca7
Nestor162 <nestorcicardini@gmail.com>
2023-02-15 15:28:43 +0100

    Aggiunti commenti

--- m1/s3/g3/homework/assets/js/script.js
+++ m1/s3/g3/homework/assets/js/script.js
@@ -1,13 +1,16 @@
 let button = document.getElementById("add");
 let container = document.getElementById("task_container");
 
+// al click del bottone scatenare la funzione 'getTask'
 button.addEventListener("click", getTask);
 
+// questa funzione ricava il testo dell'imput e 'chiama' a sua volta la funzione 'addTask'
 function getTask() {
     let task = document.getElementById("input").value;
     addTask(task);
 }
 
+// la funzione addTask crea una elemento 'li' e lo mostra sul HTML. Inoltre chiama la funzione 'clearInput'
 function addTask(currentTask) {
     let li = document.createElement("li");
     li.innerText = currentTask;
@@ -15,6 +18,9 @@ function addTask(currentTask) {
     clearInput();
 }
 
+// questa funzione semplicemente svuota la casella di input
 function clearInput() {
     document.getElementById("input").value = "";
 }
+
+function completeTask(params) {}
