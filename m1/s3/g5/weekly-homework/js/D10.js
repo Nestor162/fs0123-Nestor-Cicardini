f1deb88d45068ce884398919d5c40feb36e87f91
Nestor162 <nestorcicardini@gmail.com>
2023-02-17 16:02:49 +0100

    Esercizio 22

--- m1/s3/g5/weekly-homework/js/D10.js
+++ m1/s3/g5/weekly-homework/js/D10.js
@@ -477,6 +477,15 @@ console.log(selectTd());
   Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
 */
 
+function printTd() {
+    let colonne = document.getElementsByTagName("td");
+    for (td of colonne) {
+        console.log(td.innerText);
+    }
+}
+
+printTd();
+
 /* ESERCIZIO 23
   Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
 */
